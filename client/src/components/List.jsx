1ddc1e3882d42f1c577fafbea7d27a8896366eb8
de24world <de24world@gmail.com>
2021-03-29 00:47:46 +0200

    move to data

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { DataGrid } from "@material-ui/data-grid";
+import rows from "../../data/listdata.json";
 
 const columns = [
   { field: "offender", headerName: "Offender", width: 130 },
@@ -8,46 +9,6 @@ const columns = [
   { field: "country", headerName: "Country", width: 130 },
   { field: "city", headerName: "City", width: 130 },
   { field: "evidence", headerName: "Evidence", width: 260 },
-];
-
-const rows = [
-  {
-    id: 1,
-    offender: "black women",
-    victim: "asian women",
-    when: "2020-01-01",
-    country: "USA",
-    city: "Atlanta",
-    evidence: "https://youtu.be/dWehG6U2JOo",
-  },
-  {
-    id: 2,
-    victim: "Lannister",
-    offender: "Cersei",
-    country: "USA",
-    city: "newyork",
-  },
-  {
-    id: 3,
-    victim: "Lannister",
-    offender: "Jaime",
-    country: "Germany",
-    city: "",
-  },
-  {
-    id: 4,
-    victim: "Stark",
-    offender: "Arya",
-    country: "France",
-    city: "Paris",
-  },
-  {
-    id: 5,
-    victim: "Targaryen",
-    offender: "Daenerys",
-    country: "Germany",
-    city: "Leipzig",
-  },
 ];
 
 export default function List() {
