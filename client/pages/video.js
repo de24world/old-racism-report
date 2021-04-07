5a0e6d8c684b8a0ce45ce01ce9c694f1ea5c5543
de24world <de24world@gmail.com>
2021-04-08 00:13:51 +0200

    input search with data json

--- client/pages/video.js
+++ client/pages/video.js
@@ -1,33 +1,37 @@
 import { useState, useEffect } from "react";
 import Axios from "axios";
+
 import Layout from "../src/components/common/Layout";
 import VideoList from "../src/components/Video/VideoList";
 import VideoFilter from "../src/components/Video/VideoFilter";
 
-const ListPage = () => {
+const VideoPage = () => {
   const [dataList, setDataList] = useState([]);
+  const [q, setQ] = useState("");
   const API_URL = "http://localhost:3001/lists";
 
-  const getData = () => {
+  useEffect(() => {
     Axios.get(API_URL).then((res) => {
       console.log(res.data);
       setDataList(res.data);
     });
-  };
-
-  useEffect(() => {
-    getData();
   }, []);
+
+  function search(rows) {
+    return rows.filter((row) => row.country.toLowerCase().indexOf(q) > -1);
+  }
 
   return (
     <div>
       <Layout>
         <p>This is video.js Page</p>
+        <input type="text" value={q} onChange={(e) => setQ(e.target.value)} />
+        {/* https://youtu.be/d1r0aK5awWk */}
         <VideoFilter dataList={dataList} />
-        <VideoList dataList={dataList} />
+        <VideoList dataList={search(dataList)} />
       </Layout>
     </div>
   );
 };
 
-export default ListPage;
+export default VideoPage;
