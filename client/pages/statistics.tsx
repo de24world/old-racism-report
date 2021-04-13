7ef6cee6ee9e003db7efe24923fdac0f373048cd
de24world <de24world@gmail.com>
2021-04-13 21:36:06 +0200

    install d3

--- client/pages/statistics.tsx
+++ client/pages/statistics.tsx
@@ -1,10 +1,28 @@
+import React, { useState, useEffect } from "react";
+import * as d3 from "d3";
 import Layout from "../src/components/common/Layout";
+import Chart from "../src/components/Chart";
 
 const StatisticsPage = () => {
+  const [data, setData] = useState([]);
+  // const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    d3.json("http://localhost:3001/api").then((data) => {
+      setData(data);
+      // setLoading(false);
+    });
+    return () => undefined;
+  }, []);
+
+  console
   return (
     <div>
       <Layout>
         <p>This is statistics.js Page</p>
+        {/* {loading && <div>loading</div>}
+        {!loading &&  ... */}
+        <Chart data={data} />
       </Layout>
     </div>
   );
---
+++ client/src/components/Chart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const Chart = (props: Props) => {
+  return <div> Chart Component</div>;
+};
+
+export default Chart;
