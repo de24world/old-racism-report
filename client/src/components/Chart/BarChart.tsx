52185f533eaec389981d9eb376cf06092aa384f7
de24world <de24world@gmail.com>
2021-04-17 20:47:54 +0200

    barchart samle update

--- client/src/components/Chart/BarChart.tsx
+++ client/src/components/Chart/BarChart.tsx
@@ -1,19 +1,65 @@
 import React, { useState, useEffect, useRef } from "react";
-import * as d3 from "d3";
-import { select } from "d3";
+import { select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
 
 interface Props {}
 
 const BarChart = (props: Props) => {
-  const data = props.data;
-  // const [data, setData] = useState([25, 30, 45, 60, 20]);
+  // const data = props.data;
+  // console.log(data, 'this data is in Barchart')
+  const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
   const svgRef = useRef();
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    const xScale = scaleBand()
+      .domain(data.map((value, index) => index))
+      .range([0, 300])
+      .padding(0.5);
+
+    const yScale = scaleLinear().domain([0, 150]).range([150, 0]);
+
+    const colorScale = scaleLinear()
+      .domain([75, 100, 150])
+      .range(["green", "orange", "red"])
+      .clamp(true);
+
+    const xAxis = axisBottom(xScale).ticks(data.length);
+
+    svg.select(".x-axis").style("transform", "translateY(150px)").call(xAxis);
+
+    const yAxis = axisRight(yScale);
+    svg.select(".y-axis").style("transform", "translateX(300px)").call(yAxis);
+
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+
+      .style("transform", "scale(1, -1)")
+      .attr("x", (value, index) => xScale(index))
+      .attr("y", -150)
+      .attr("width", xScale.bandwidth())
+      .transition()
+      .attr("fill", colorScale)
+      .attr("height", (value) => 150 - yScale(value));
+  }, [data]);
 
   console.log(data, "data in Barchart");
 
   return (
     <div>
-      <svg ref={svgRef}>Sample SVG</svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+      <button onClick={() => setData(data.map((value) => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter((value) => value < 35))}>
+        Filter data
+      </button>
     </div>
   );
 };
