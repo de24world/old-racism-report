0b00fdea2a7b27e8f66444470f659089371e3a16
de24world <de24world@gmail.com>
2021-05-04 11:56:30 +0200

    add radio form

--- client/src/components/Contact/Form.tsx
+++ client/src/components/Contact/Form.tsx
@@ -15,6 +15,8 @@ import {
   MenuItem,
   Button,
 } from "@material-ui/core/";
+import Alert from "@material-ui/lab/Alert";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 import Country from "./FormGroup/Country";
@@ -23,14 +25,14 @@ import DataForm from "./FormGroup/DateForm";
 type FormValues = {
   // https://codesandbox.io/s/react-hook-form-v6-controller-ts-jwyzw?file=/src/index.tsx:1129-1496
   // Native: string;
-  // TextField: string;
+  TextField: string;
   // Select: string;
   // ReactSelect: NestedValue<{ value: string; label: string }>;
   // Checkbox: boolean;
   // switch: boolean;
   // RadioGroup: string;
   // MUI_Slider: string;
-  Datepicker: Date;
+  // Datepicker: Date;
   // numberFormat: number;
   // downShift: string;
   // country: NestedValue<{ code: string; label: string; phone: string }>;
@@ -42,6 +44,10 @@ const useStyles = makeStyles((theme) => ({
       margin: theme.spacing(1),
     },
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
 }));
 
 const Form = () => {
@@ -49,29 +55,60 @@ const Form = () => {
 
   const {
     control,
+    reset,
+    setValue,
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
 
   return (
     <>
       {/* <Country /> */}
       대량 데이터는 이메일로 보내주세요
       <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          label="Youtube URL"
-          placeholder="example : https://youtu.be/dWehG6U2JOo"
-          fullWidth
-          variant="outlined"
-          {...register("evidence", { required: true })}
+        <section>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel>Offender</InputLabel>
+            <Controller
+              name="offender"
+              control={control}
+              defaultValue=""
+              rules={{ required: true }}
+              render={({ field }) => (
+                <Select label="Offender" {...field}>
+                  <MenuItem value="black women">Black women</MenuItem>
+                  <MenuItem value="black guy">Black guy</MenuItem>
+                  <MenuItem value="white guy">White guy</MenuItem>
+                </Select>
+              )}
+            />
+          </FormControl>
+          {errors.offender && <Alert severity="error">This is required</Alert>}
+        </section>
+
+        <Controller
+          name="evidence"
+          control={control}
+          defaultValue=""
+          rules={{ required: true }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Youtube URL"
+              variant="outlined"
+              fullWidth
+            />
+          )}
         />
+        {errors.evidence && <Alert severity="error">This is required</Alert>}
 
         {/* <DataForm /> */}
-        <Controller
+        {/* <Controller
           control={control}
           name="Datepicker"
           render={({ field }) => (
@@ -84,7 +121,25 @@ const Form = () => {
               placeholderText="Select date"
             />
           )}
-        />
+        /> */}
+
+        <section>
+          <label>Level</label>
+          <Controller
+            name="level"
+            control={control}
+            defaultValue=""
+            rules={{ required: true }}
+            render={({ field }) => (
+              <RadioGroup {...field}>
+                <FormControlLabel value="1" control={<Radio />} label="1" />
+                <FormControlLabel value="2" control={<Radio />} label="2" />
+                <FormControlLabel value="3" control={<Radio />} label="3" />
+              </RadioGroup>
+            )}
+          />
+          {errors.level && <Alert severity="error">This is required</Alert>}
+        </section>
 
         <Button type="submit" variant="contained" color="primary">
           Submit
