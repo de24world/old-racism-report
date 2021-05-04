5b693e913e7834e66b0f9b3d87157aa90bd7a72d
de24world <de24world@gmail.com>
2021-05-04 02:00:14 +0200

    update sample datepicker

--- client/src/components/Contact/Form.tsx
+++ client/src/components/Contact/Form.tsx
@@ -1,7 +1,40 @@
-import React from "react";
-import { TextField } from "@material-ui/core/";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import {
+  TextField,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  InputLabel,
+  RadioGroup,
+  Radio,
+  Select,
+  MenuItem,
+  Button,
+} from "@material-ui/core/";
 import { makeStyles } from "@material-ui/core/styles";
+
 import Country from "./FormGroup/Country";
+import DataForm from "./FormGroup/DateForm";
+
+type FormValues = {
+  // https://codesandbox.io/s/react-hook-form-v6-controller-ts-jwyzw?file=/src/index.tsx:1129-1496
+  // Native: string;
+  // TextField: string;
+  // Select: string;
+  // ReactSelect: NestedValue<{ value: string; label: string }>;
+  // Checkbox: boolean;
+  // switch: boolean;
+  // RadioGroup: string;
+  // MUI_Slider: string;
+  Datepicker: Date;
+  // numberFormat: number;
+  // downShift: string;
+  // country: NestedValue<{ code: string; label: string; phone: string }>;
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,10 +47,49 @@ const useStyles = makeStyles((theme) => ({
 const Form = () => {
   const classes = useStyles();
 
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
   return (
     <>
-      <Country />
+      {/* <Country /> */}
       대량 데이터는 이메일로 보내주세요
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          label="Youtube URL"
+          placeholder="example : https://youtu.be/dWehG6U2JOo"
+          fullWidth
+          variant="outlined"
+          {...register("evidence", { required: true })}
+        />
+
+        {/* <DataForm /> */}
+        <Controller
+          control={control}
+          name="Datepicker"
+          render={({ field }) => (
+            <DatePicker
+              {...field}
+              selected={field.value}
+              dateFormat="MM/yyyy"
+              showMonthYearPicker
+              showFullMonthYearPicker
+              placeholderText="Select date"
+            />
+          )}
+        />
+
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
     </>
   );
 };
---
+++ client/src/components/Contact/FormGroup/DateForm.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { TextField } from "@material-ui/core";
+import { useForm, Controller } from "react-hook-form";
+
+type FormValues = {
+  DatePicker: string;
+};
+
+const DateForm = () => {
+  const { handleSubmit, control } = useForm<FormValues>();
+  const [startDate, setStartDate] = useState(new Date());
+
+  return (
+    <>
+      {/* <form onSubmit={handleSubmit((data) => console.log(data))}> */}
+      <Controller
+        control={control}
+        name="DatePicker"
+        render={({ field: { onChange, onBlur, value, ref } }) => (
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="MM/yyyy"
+            showMonthYearPicker
+            showFullMonthYearPicker
+          />
+        )}
+      />
+      {/* 
+        <input type="submit" />
+      </form> */}
+    </>
+  );
+};
+
+export default DateForm;
