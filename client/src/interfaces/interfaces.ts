d04f75769ba3c94a98cafbc2955a7286c640b699
de24world <de24world@gmail.com>
2021-05-11 21:39:19 +0200

    sample interface update

---
+++ client/src/interfaces/interfaces.ts
@@ -0,0 +1,12 @@
+export interface IDataProps {
+  id: string,
+  offender: string,
+  victim: string,
+  time: any, 
+  // change later moment
+  country: string,
+  contryCode: string,
+  city: string,
+  evidence: string,
+  level: number
+}
