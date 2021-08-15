327a2bc62d73b284d142db41ceb5e7d247de346a
HoSoo Lee <de24world@gmail.com>
2021-08-16 00:57:16 +0200

    Update database config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("https://racism-report-server.herokuapp.com/"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd1e75d8c4beba5d0861169155da51a0a'),
+      secret: env("ADMIN_JWT_SECRET", "d1e75d8c4beba5d0861169155da51a0a"),
     },
   },
 });
